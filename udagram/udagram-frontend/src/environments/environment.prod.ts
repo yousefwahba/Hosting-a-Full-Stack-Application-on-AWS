564fc5e1913e37ac5b66d8410404e3b9b397ce78
yousefwahba <yousefwahba.official@gmail.com>
2023-02-03 17:09:47 +0200

    fix build bug and add eb link to frontend

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: true,
-  appName: "Udagram",
-  apiHost: "http://localhost:8080/api/v0",
+  appName: 'Udagram',
+  apiHost:
+    'http://udagram-api-dev.eba-8upprdzq.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
